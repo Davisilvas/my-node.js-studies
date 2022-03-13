b8866b44b32d5bc1f6ad5c1d4101fa20740f91ab
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-12 22:24:59 -0300

    program thar recives arguments on the prompt

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-console.log("Hello World");
+// let args = process.argv.slice(2)
 
-let n1 = 5.4 ;
-let n2 = 8.8 ;
-let mf = (n1+n2)/2;
+// let n1 = Number(args[0]);
+// let n2 = Number(args[1]);
+// let mf = media(n1, n2);
 
-console.log(mf);
+// function media(x, y){
+//     return (x + y)/2;
+// }
 
-// console.log(document);
+// console.log(mf);
+
+// console.log(process.argv);
+
+let args = process.argv.slice(2)
+
+let a = Number(args[1]);
+let b = Number(args[2]);
+let c = " ";
+
+if (args[0] == 's'){
+    c = soma(a,b);
+}else if(args[0] =='m'){
+    c = mult(a,b);
+}else{
+    c = "houve um erro"
+};
+
+function soma(x, y){
+    return x + y;
+};
+
+function mult(x,y){
+    return x * y;
+};
+
+console.log(c);
