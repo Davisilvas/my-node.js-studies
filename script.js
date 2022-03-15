27352384f3ce0ddd2fe53922a2fad4a75b986407
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-14 21:28:49 -0300

    aprendi a usar os módulos do prórprio módulo

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-let calc = require("./calc");
 
-let mult = require('./calc').mult;
+// fs vem de file system
 
-console.log(mult(70, 88));
+// fs.writeFile('teste.txt', 'hello world', (error) => {
+//     if(error){throw error}
+//     console.log("arquivo criado com sucesso")
+// })
 
-console.log(calc.soma(70, 88));
+// fs.appendFile('teste.txt' , ' - olá mundo' , (error)=>{
+//     if(error){throw error}
+//     console.log("arquivo atualizado com sucesso")
+// });
+
+// fs.unlink('teste.txt', (error) => {
+//     if (error) {throw error}
+//     console.log('arquivo deletado com sucesso')
+// })
+
+// fs.rename('teste.txt' , 'novoNome.txt' , (error) => {
+//     if (error){throw error}
+//     console.log('nome do arquivo alterado com sucesso')
+// })
+
+// fs.readFile('novoNome.txt' , (error, data) => {
+//     if(error){throw error}
+//     console.log(data)
+// })
+
+// fs.readFile('novoNome.txt', 'UTF8' , (error, data) => {
+//     if(error){throw error}
+//     console.log(data)
+// })
+
+
+let fs = require('fs');
+let args = process.argv.slice(2);
+
+let fileName = args[0];
+
+fs.readFile(fileName, 'UTF8', (error, data) => {
+    if (error){throw error}
+
+    fs.writeFile (fileName + "_Uppercase", data.toUpperCase(), (error)=>{
+        if (error){throw error}
+    })
+    console.log("Arquivo gerado com sucesso")
+})
