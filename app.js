451a50adb62c093216cf7b962679ef97ac55849d
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-22 10:52:19 -0300

    request

--- app.js
+++ app.js
@@ -3,26 +3,42 @@ const url = require('url');
 const fs = require('fs');
 
 
-http.createServer((req, res) =>{
+function handleFile (req, res, callback){
 
     let path = url.parse(req.url).pathname;
 
-    if (path == "" || path =="/"){
-        path = "./index.html";
-    }
     let fileName = "." + path;
 
     fs.readFile(fileName, (error, data) =>{
         if (error){
-            res.writeHead(404, { "content-type": "text/html;charset=utf-8"});
-            res.end("<h1>Página não encontrada</h1>");
+                if (callback){
+                   if (!callback(req, res)){
+                    res.writeHead(404, { "content-type": "text/html;charset=utf-8"});
+                    res.end("<h1>Página não encontrada</h1>");                       
+                   }
+                }
         }else{
             res.writeHead(200, {"content-type": "text/html"});
             res.write(data);
             res.end();
         }
     })
+}
 
+function handleRequest(req, res){
+    let path = url.parse(req.url).pathname;
+    
+    if (path == "/teste"){
+        res.end("teste");
+        return true;
+    }
+    return false;
+
+}
+
+http.createServer((req, res) =>{
+
+    handleFile(req, res, handleRequest);
 
 }).listen(3000, (error)=>{
     if(error){
@@ -31,3 +47,38 @@ http.createServer((req, res) =>{
         console.log("servidor rodando na porta 3000")
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if (path == "" || path =="/"){
+    //     path = "./index.html";
+    // }
+    // let fileName = "." + path;
+
+    // fs.readFile(fileName, (error, data) =>{
+    //     if (error){
+    //         if (path == "/teste"){
+    //             res.end("teste");
+    //             return;
+    //         }
+
+    //         res.writeHead(404, { "content-type": "text/html;charset=utf-8"});
+    //         res.end("<h1>Página não encontrada</h1>");
+    //     }else{
+    //         res.writeHead(200, {"content-type": "text/html"});
+    //         res.write(data);
+    //         res.end();
+    //     }
+    // })
+
